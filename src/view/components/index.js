c57fb019024d1cbc9237cf5b00834a594a5a5ea9
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2021-07-28 19:56:18 -0300

    FInalizar componente Confirm

--- src/view/components/index.js
+++ src/view/components/index.js
@@ -1,5 +1,6 @@
 import Loading from "./Loading";
 import Notify from "./Notify";
 import Alert from "./Alert";
+import Confirm from "./Confirm";
 
-export { Loading, Notify, Alert };
+export { Loading, Notify, Alert, Confirm };
